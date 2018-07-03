0ae35dbe21341ea8505b261f31282b0a5349f05c
ixnp <enprocopios@gmail.com>
2018-07-02 19:42:21 -0700

    duplicates in string

---
+++ duplicatesInString.js
@@ -0,0 +1,21 @@
+
+function hasDuplicate(str){
+  let dupObj = {};
+  if(typeof str != 'string') return false;
+  for(let i = 0; i < str.length; i++){
+    let current = str[i];
+    if(!dupObj[current]){
+      dupObj[current]= true;
+    }
+    else{ return false;}
+  }
+  return 'no duplicates';
+}
+//should return false 
+hasDuplicate('aabcd');
+//should return 'no duplicates'
+hasDupes('abcdefg');
+//should return 'no duplicates'
+hasDuplicate('');
+//should return false 
+hasDuplicate(5);

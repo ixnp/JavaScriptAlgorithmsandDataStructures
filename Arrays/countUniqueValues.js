6eb97f73b4df78f2fb1cfa2e92326b86e3e5d04c
ixnp <enprocopios@gmail.com>
2019-02-08 14:15:59 -0800

    countUniqueVal

---
+++ Arrays/countUniqueValues.js
@@ -0,0 +1,22 @@
+//Use two pointers, advice one through the array (i) and leave one at arr[0] (j)
+//If arr[j] does not match arr[i] advance j and assign arr[j] the value of arr[i]
+//return the index of j +1 to get the number of unique values. 
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != arr[j]) {
+      j++
+      arr[j] = arr[i]
+
+      console.log('hi', i)
+    }
+
+  }
+  return j + 1;
+}
+
+countUniqueValues([1, 1, 1, 2, 2, 3, 3, 4, 5, 6]); //6
+countUniqueValues([]); //0
+countUniqueValues([1, 1, 1, 1, 1, 1, 1]); //1

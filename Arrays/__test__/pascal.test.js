bd4bf7c1befad53b6d4bb9eaba7aa478f071719e
ixnp <enprocopios@gmail.com>
2019-04-05 16:40:46 -0700

    added pascal test

--- Arrays/__test__/pascal.test.js
+++ Arrays/__test__/pascal.test.js
@@ -10,13 +10,25 @@ const results = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
 
 
 
-describe("Pascal", () => {
-  it("function will print out console logs", () => {
+describe("Will print the pascal triangle", () => {
+  it("function will print a console logs", () => {
     expect(spyLog).toHaveBeenCalled();
   });
 
   it("will return a 2d array matching results", () => {
     expect(results).toEqual(expect.arrayContaining(expected));
   });
-  
+
+  it("the first array will be 1", () => {
+    let expected = true;
+    //runs through array, if the start or end 
+    //does not === 1 expected will turn to false 
+    for (let i = 0; i < expected.length; i++){
+      if (arr[i][0] !== 1 || arr[i][arr.length - 1] !== 1){
+        expected = false;
+        break;
+      }
+    }
+    expect(expected).toBeTruthy();
+  });
 });
